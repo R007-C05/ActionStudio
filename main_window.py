363e2c7cbdf14c6d4c5727d6cecd033044c8c896
TheTatsujin <luis.martin119@alu.ulpgc.es>
2026-03-28 12:46:28 +0000

    First commit

---
+++ main_window.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+from PySide6.QtWidgets import QMainWindow
+from ui_form import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Action Studio")
